0b9f708673fe1dc08900d58a466cafccbd4dbd8f
Tokuhiro Matsuno <tokuhirom@gmail.com>
2024-05-30 11:14:58 +0900

    comment

--- target/preload.js
+++ target/preload.js
@@ -1,5 +1,13 @@
 const { contextBridge, ipcRenderer } = require('electron')
 
+// I want to write this file in kotlin! But I can't.
+//
+// Because:
+// - Electron doesn't support ESM modules
+// - Kotlin doesn't support webpack for NodeJS.
+//
+// As a result, I can't write preload script in kotlin.
+
 contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
